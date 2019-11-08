d8bf8497a7050a594310279db507101d282e09d3
akbel <colakakbel@gmail.com>
2019-11-08 14:48:52 +0100

    practice prob- project.prep appJs- first 3

--- week-2-project/app.js
+++ week-2-project/app.js
@@ -11,45 +11,54 @@
 const object = {
   entries: {},
   isPrimitive: function (value) {
-    // write me!
+    
+    return (value !== Object(value));
   },
   hasKey: function (obj, key) {
-    // write me!
+  
+    return (obj.hasOwnProperty(key));
   },
   hasValue: function (obj, value) {
-    // write me!
+    
+    return (Object.values(obj).indexOf(value) > -1);
   },
   addEntry: function (key, value) {
-    if (null) { // write me!
+    if (typeof key !== 'string') {
       return new TypeError('addEntry: key should be a string');
     }
-    if (null) { // write me! (using this.isPrimitive)
+    if (this.isPrimitive !== value) { // write me! (using this.isPrimitive)
       return new TypeError('addEntry: value should be a primitive');
     }
+    // what I tried:
+    // if (this.hasOwnProperty(key) === true){}
+    // if (this.entries.hasOwnProperty(key) === true){}
+    // if (this.hasKey(this.entries, key)){}
+    // if (this.entries.hasKey(key)) {}
+    // if (this.entries.hasKey(this.entries, key)) {}
     if (null) { // write me! (using this.hasKey)
       return { [key]: new Error(`addEntry: key "${key}" already exists`) };
     }
 
-    // write me!
+    else {return }
   },
   removeEntry: function (key) {
-    if (null) { // write me!
+    if (typeof key !== 'string') { // write me!
       return new TypeError('removeEntry: key should be a string');
     }
-    if (null) { // write me! (using this.hasKey)
+    if (!this.hasKey(this.entries, key)) { // write me! (using this.hasKey)
       return { [key]: new ReferenceError(`removeEntry: no property "${key}" in this.entries`) };
     }
 
-    // write me!
+    
   },
   updateEntry: function (key, value) {
-    if (null) { // write me!
+    if (typeof key !== 'string') { // write me!
       return new TypeError('updateEntry: key should be a string');
     }
-    if (null) { // write me! (using this.isPrimitive)
+    if (this.isPrimitive !== value) { // write me! (using this.isPrimitive)
       return new TypeError('updateEntry: value should be a primitive');
     }
-    if (null) { // write me! (using this.hasKey)
+    if (!this.hasKey(this.entries, key)) { // write me! (using this.hasKey)
       return { [key]: new ReferenceError(`updateEntry: no property "${key}" in this.entries`) };
     }
 
@@ -59,7 +68,7 @@ const object = {
     // write me!
   },
   findByKey: function (key) {
-    if (null) { // write me!
+    if () { // write me!
       return new TypeError('findByKey: key should be a string');
     }
     if (null) { // write me! (using this.hasKey)
@@ -69,9 +78,11 @@ const object = {
     // write me!
   },
   findByValue: function (value) {
-    if (null) { // write me! (using this.isPrimitive)
+    if (this.isPrimitive !== value) { // write me! (using this.isPrimitive)
       return new TypeError('findByValue: value should be a primitive');
     }
+    // if (typeof this.hasValue === value)
+    // if (typeof value !== 'this.hasValue')
     if (null) { // write me! (using this.hasValue)
       return new ReferenceError(`findByValue: no entry with value (${typeof value}, ${value})`);
     }
--- week-2-project/practice-problems/project-prep.js
+++ week-2-project/practice-problems/project-prep.js
@@ -21,8 +21,17 @@ try {
     { name: 'tenth', args: [new WeakMap()], expected: false },
   ];
   function isPrimitive(thing) {
-    // write me!
-  }
+    // let type = typeof thing;
+    // return isPrimitive(type);
+    
+    //if (type !== "object" || type !== "function"){ return true; } // 1,2,3,4,5 passes
+    // if (type == "object" || type == "function") {return false;} // non of them passed
+    // return false;
+
+    return (thing !== Object(thing));
+    
+    }
+
   isPrimitive.display = true;
   evaluate(isPrimitive, isPrimitiveTests);
 
@@ -38,6 +47,7 @@ try {
   ];
   function hasKey(obj, key) {
     // write me!
+  return (obj.hasOwnProperty(key)); 
   }
   hasKey.display = true;
   evaluate(hasKey, hasKeyTests);
@@ -55,7 +65,9 @@ try {
   function hasValue(obj, value) {
     // write me!
     // consider using Object.keys, .filter and obj.hasOwnProperty
-  }
+    //stackoverflow 
+    return (Object.values(obj).indexOf(value) > -1)} // if the value is not in the object, write -1. othervise write the index number
+      
   hasValue.display = true;
   evaluate(hasValue, hasValueTests);
 
@@ -65,21 +77,25 @@ try {
 
   const modifyToObjectWithBracketsTests = [
     { name: 'first', args: [firstObj, 'x', 0], expected: { a: 0, x: 0 } },
-    { name: 'second', args: [firstObj, 'x', 0], expected: { a: 0, x: 0 } },
+    { name: 'second',args: [firstObj, 'x', 0], expected: { a: 0, x: 0 } },
     { name: 'third', args: [secondObj, 'a', 1], expected: { a: 1, b: 1 } },
-    { name: 'fourth', args: [secondObj, 'a', 1], expected: { a: 1, b: 1 } },
-    { name: 'fifth', args: [{}, 'b', 'hi!'], expected: { b: 'hi!' } },
+    { name: 'fourth',args: [secondObj, 'a', 1], expected: { a: 1, b: 1 } }, // when a copied key changes, original property key also changed
+    { name: 'fifth', args: [{}, 'b', 'hi!'], expected: { b: 'hi!' } },  //a new key with value added
   ];
   function modifyToObjectWithBrackets(obj, key, value) {
     // write me!
     // (remember to avoid side effects)
+    
+    let newobj = Object.assign({},obj); //const newobj = {...obj}
+    newobj[key] = value;
+    return newobj;
   }
   modifyToObjectWithBrackets.display = true;
   evaluate(modifyToObjectWithBrackets, modifyToObjectWithBracketsTests)
 
   const deleteFromObjectTests = [
-    { name: 'first', args: [firstObj, 'x'], expected: { a: 0 } },
-    { name: 'second', args: [firstObj, 'a'], expected: {} },
+    { name: 'first', args: [firstObj, 'x'], expected: { a: 0 } }, //if the object doesnt contain the arg, do nothing
+    { name: 'second', args: [firstObj, 'a'], expected: {} },      // if arg is in the object delete it(key and value)
     { name: 'third', args: [firstObj, 'x'], expected: { a: 0 } },
     { name: 'fourth', args: [secondObj, 'a'], expected: { b: 1 } },
     { name: 'fifth', args: [secondObj, 'b'], expected: { a: 0 } },
@@ -88,31 +104,60 @@ try {
   function deleteFromObject(obj, key) {
     // write me!
     // (remember to avoid side effects)
+
+    const newObject = {...obj};
+    delete newObject[key];
+    return newObject;
   }
   deleteFromObject.display = true;
   evaluate(deleteFromObject, deleteFromObjectTests)
 
 
   const findByKeyTests = [
-    { name: 'first', args: [firstObj, 'x'], expected: {} },
-    { name: 'second', args: [firstObj, 'a'], expected: { a: 0 } },
+    { name: 'first', args: [firstObj, 'x'], expected: {} }, //there is no such key as x, so return empty
+    { name: 'second',args: [firstObj, 'a'], expected: { a: 0 } },// if there is key as the same name as arg return the key with its value
     { name: 'third', args: [firstObj, 'a'], expected: { a: 0 } },
-    { name: 'fourth', args: [secondObj, 'a'], expected: { a: 0 } },
+    { name: 'fourth',args: [secondObj, 'a'], expected: { a: 0 } },
     { name: 'fifth', args: [secondObj, 'b'], expected: { b: 1 } },
     { name: 'fifth', args: [{ b: 'hi!' }, 'b'], expected: { b: 'hi!' } },
   ];
   function findByKey(obj, key) {
     // write me!
     // (remember to avoid side effects)
+    /* 'in' operator for property existance check
+    let user = { name: "John", age: 30 };
+    alert( "age" in user ); // true, user.age exists
+    alert( "blabla" in user ); // false, user.blabla doesn't exist */
+
+    const findObj = {...obj};
+    if (key in findObj === true){ 
+      let newfind = {};
+      let value = findObj[key];
+      newfind[key] = value;
+      return newfind;
+    }
+    /*
+    if (key in findObj === true){  
+      return findObj; } //2,3,5 passes
+    
+      and next I tried to find by value :
+      else if(key in findObj === true && key[value] in findObj !== true){
+      let newfind = findObj;
+      return newfind;} */
+
+    else { // 1 pas
+      return {};
+    }
+
   }
   findByKey.display = true;
   evaluate(findByKey, findByKeyTests)
 
 
   const findByValueTests = [
-    { name: 'first', args: [firstObj, 0], expected: { a: 0 } },
-    { name: 'second', args: [firstObj, 1], expected: {} },
-    { name: 'third', args: [firstObj, 0], expected: { a: 0 } },
+    { name: 'first', args: [firstObj, 0], expected: { a: 0 } }, // arg is in our property value, so write the key and value
+    { name: 'second', args: [firstObj, 1], expected: {} }, // we have no such prop value, {}
+    { name: 'third', args: [firstObj, 0], expected: { a: 0 } }, 
     { name: 'fourth', args: [secondObj, 0], expected: { a: 0 } },
     { name: 'fifth', args: [secondObj, 1], expected: { b: 1 } },
     { name: 'fifth', args: [{ b: 'hi!', c: 'hi!' }, 'hi!'], expected: { b: 'hi!', c: 'hi!' } },
@@ -120,6 +165,30 @@ try {
   function findByValue(obj, value) {
     // write me!
     // (remember to avoid side effects)
+
+    /*const valueObject = {...obj};
+    if (value in valueObject === true){
+      return valueobject;
+    }
+    else {return {};} // only 2 passed
+    */
+
+   const valueObject = JSON.parse(JSON.stringify(obj)); // 1,3,5 passes
+  // return valueObject;
+
+
+   /* */
+    if (value in Object.values(valueObject)){ // 1,3 passes
+    return valueObject;}
+    else {return {};} // 2 passes 
+
+    /*
+    let key = value[obj];
+    for (let key in firstObj){ return valueObject;} // 1,3,6 passes
+    */
+    
+    
+
   }
   findByValue.display = true;
   evaluate(findByValue, findByValueTests)
