680fee6b6e17cc93dfc0967af4e7cdfb8c339137
akbel <colakakbel@gmail.com>
2019-11-10 13:18:41 +0100

    an example of doing git from vsc

--- week-2-project/app.js
+++ week-2-project/app.js
@@ -8,7 +8,7 @@
   - and users can access & modify that data
 */
 
-
+// try to make a change to merge conflicts
 
 
 const object = {
@@ -33,7 +33,6 @@ const object = {
     if (this.isPrimitive(value) === false) { // write me! (using this.isPrimitive)
       return new TypeError('addEntry: value should be a primitive');
     }
-<<<<<<< HEAD
     // what I tried:
     // if (this.hasOwnProperty(key) === true){}
     // if (this.entries.hasOwnProperty(key) === true){}
@@ -43,10 +42,6 @@ const object = {
     
     if (key in this.entries===true) { // write me! (using this.hasKey)
       return { [key]: new Error(`addEntry: key "${key}" already exists`) };
-=======
-    if (null) { // write me! (using this.hasKey)
-      return new Error(`addEntry: key "${key}" already exists`);
->>>>>>> upstream/master
     }
     else {this.entries[key] = value;
       return true;}
@@ -55,22 +50,12 @@ const object = {
     if (typeof key !== 'string') { // write me!
       return new TypeError('removeEntry: key should be a string');
     }
-<<<<<<< HEAD
     if (!this.hasKey(this.entries, key)) { // write me! (using this.hasKey)
       return { [key]: new ReferenceError(`removeEntry: no property "${key}" in this.entries`) };
     }
     delete this.entries[key];
     return true;
     
-=======
-    if (null) { // write me! (using this.hasKey)
-      return new ReferenceError(`removeEntry: no property "${key}" in this.entries`);
-    }
-
-    delete this.entries[key]
-    return true
-    // write me!
->>>>>>> upstream/master
   },
   
   updateEntry: function (key, value) {
@@ -80,13 +65,8 @@ const object = {
     if (!this.isPrimitive(value)) { // write me! (using this.isPrimitive)
       return new TypeError('updateEntry: value should be a primitive');
     }
-<<<<<<< HEAD
     if (!this.hasKey(this.entries, key)) { // write me! (using this.hasKey)
       return { [key]: new ReferenceError(`updateEntry: no property "${key}" in this.entries`) };
-=======
-    if (null) { // write me! (using this.hasKey)
-      return new ReferenceError(`updateEntry: no property "${key}" in this.entries`);
->>>>>>> upstream/master
     }
     else { this.entries[key] = value;
       return true;}
@@ -102,13 +82,8 @@ const object = {
     if (typeof key!== 'string') { // write me!
       return new TypeError('findByKey: key should be a string');
     }
-<<<<<<< HEAD
     if (!this.hasKey(this.entries,key)) { // write me! (using this.hasKey)
       return { [key]: new ReferenceError(`findByKey: no property "${key}" in this.entries`) };
-=======
-    if (null) { // write me! (using this.hasKey)
-      return new ReferenceError(`findByKey: no property "${key}" in this.entries`);
->>>>>>> upstream/master
     }
 
     //all steps
